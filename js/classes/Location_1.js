b0bc94371cd906a87e200d8fabcd19183a23c54b
Dmitrii <dimaarion@gmail.com>
2025-03-20 06:09:56 +0200

    https://github.com/dimaarion/tanks.git

--- js/classes/Location_1.js
+++ js/classes/Location_1.js
@@ -11,7 +11,7 @@ export default class Location_1 extends Phaser.Scene{
   create(){
     this.map = this.make.tilemap({key: 'map'});
     let tiles = this.map.addTilesetImage("location_1", "tiles");
-    this.layer = this.map.createLayer("ground", tiles, this.game.config.width / 2 - 320, 0);
+    this.layer = this.map.createLayer("ground", tiles, 0, 0);
     this.layer.setCollisionByProperty({collides: true});
     this.map.setCollisionByExclusion(-1, true);
     this.matter.world.setBounds(this.game.config.width / 2 - 320, this.map.heightInPixels);
